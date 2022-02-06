ccb318d7fbfd39be011c25448bc179a2447e750a
Javier <Javier@DESKTOP-82RJPB2.home>
2022-02-06 20:22:53 +0100

    eldeposito esta lleno

---
+++ practica/src/DepositoCombustibleTest.java
@@ -0,0 +1,55 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DepositoCombustibleTest {
+
+	@Test
+	public void testGetDepositoNivel() {
+		DepositoCombustible tank = new DepositoCombustible(10.0, 12.0);
+
+		System.out.println("The tank with posin level " + tank.getDepositoNivel()) ;
+		}
+
+	@Test
+	public void testGetDepositoMax() {
+		DepositoCombustible tank = new DepositoCombustible(10.0, 12.0);
+		System.out.print("The tank with capacity " + tank.getDepositoMax()) ;
+	}
+
+	@Test
+	public void testEstaVacio() {
+		DepositoCombustible tank = new DepositoCombustible(10.0, 12.0);
+		boolean lleno = false;
+		if( tank.fill(0.0) == tank.getDepositoMax()){
+		System.out.println(lleno);
+		}else System.out.println(lleno = true);
+		
+	}
+
+	@Test
+	public void testEstaLleno() {
+		DepositoCombustible tank = new DepositoCombustible(10.0, 12.0);
+		boolean lleno = false;
+		if( tank.fill(10.0) == tank.getDepositoMax()){
+		System.out.println(lleno);
+		}else lleno = true;
+		System.out.println("es correcto");
+	}
+
+	@Test
+	public void testFill() {
+		DepositoCombustible tank = new DepositoCombustible(30.0, 04.0);
+		boolean lleno = false;
+		if( tank.fill(36.0) == tank.getDepositoNivel()){
+		System.out.println(lleno+ "el deposito esta parcial");
+		}else lleno = true;
+		System.out.println("el deposito esta lleno");
+	}
+
+	@Test
+	public void testConsumir() {
+		fail("Not yet implemented");
+	}
+
+}
